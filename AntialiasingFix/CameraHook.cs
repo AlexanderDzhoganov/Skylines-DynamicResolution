546ae2f2059d405e479f51ae72ebabc40b2318a8
Alexander Dzhoganov <alexander.dzhoganov@gmail.com>
2015-03-17 15:30:51 +0200

    version 1.11

--- AntialiasingFix/CameraHook.cs
+++ AntialiasingFix/CameraHook.cs
@@ -30,7 +30,10 @@ public class CameraHook : MonoBehaviour
 
     public Configuration config;
     public CameraController cameraController;
-    
+
+    private Texture2D bgTexture;
+    private GUISkin skin;
+
     public float GetSSAAFactor()
     {
         if (undergroundCamera.cullingMask != 0)
@@ -55,6 +58,10 @@ public class CameraHook : MonoBehaviour
         SaveConfig();
 
         cameraController = FindObjectOfType<CameraController>();
+
+        bgTexture = new Texture2D(1, 1);
+        bgTexture.SetPixel(0, 0, Color.grey);
+        bgTexture.Apply();
     }
 
     public void SaveConfig()
@@ -187,9 +194,39 @@ public class CameraHook : MonoBehaviour
 
     void OnGUI()
     {
+        if (skin == null)
+        {
+            skin = ScriptableObject.CreateInstance<GUISkin>();
+            skin.box = new GUIStyle(GUI.skin.box);
+            skin.button = new GUIStyle(GUI.skin.button);
+            skin.horizontalScrollbar = new GUIStyle(GUI.skin.horizontalScrollbar);
+            skin.horizontalScrollbarLeftButton = new GUIStyle(GUI.skin.horizontalScrollbarLeftButton);
+            skin.horizontalScrollbarRightButton = new GUIStyle(GUI.skin.horizontalScrollbarRightButton);
+            skin.horizontalScrollbarThumb = new GUIStyle(GUI.skin.horizontalScrollbarThumb);
+            skin.horizontalSlider = new GUIStyle(GUI.skin.horizontalSlider);
+            skin.horizontalSliderThumb = new GUIStyle(GUI.skin.horizontalSliderThumb);
+            skin.label = new GUIStyle(GUI.skin.label);
+            skin.scrollView = new GUIStyle(GUI.skin.scrollView);
+            skin.textArea = new GUIStyle(GUI.skin.textArea);
+            skin.textField = new GUIStyle(GUI.skin.textField);
+            skin.toggle = new GUIStyle(GUI.skin.toggle);
+            skin.verticalScrollbar = new GUIStyle(GUI.skin.verticalScrollbar);
+            skin.verticalScrollbarDownButton = new GUIStyle(GUI.skin.verticalScrollbarDownButton);
+            skin.verticalScrollbarThumb = new GUIStyle(GUI.skin.verticalScrollbarThumb);
+            skin.verticalScrollbarUpButton = new GUIStyle(GUI.skin.verticalScrollbarUpButton);
+            skin.verticalSlider = new GUIStyle(GUI.skin.verticalSlider);
+            skin.verticalSliderThumb = new GUIStyle(GUI.skin.verticalSliderThumb);
+            skin.window = new GUIStyle(GUI.skin.window);
+            skin.window.normal.background = bgTexture;
+            skin.window.onNormal.background = bgTexture;
+        }
+
         if (showConfigWindow)
         {
+            var oldSkin = GUI.skin;
+            GUI.skin = skin;
             windowRect = GUI.Window(12412, windowRect, DoConfigWindow, "Dynamic resolution");
+            GUI.skin = oldSkin;
         }
     }
 
